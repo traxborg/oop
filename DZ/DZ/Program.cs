20f597f81a0e8780a4ebc940c03a3884ecb745ef
Ura <urastislo@gmail.com>
2025-05-22 21:53:39 +0300

    putsa

---
+++ DZ/DZ/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            TextContainer text = new TextContainer();
+
+            text.AddLine("Перший рядок");
+            text.AddLine("Другий рядок");
+            text.AddLine("Третій рядок");
+
+            Console.WriteLine("Кількість рядків: " + text.LineCount);  
+
+            
+            Console.WriteLine("Рядок 1: " + text[0]); 
+
+           
+            text[1] = "Оновлений другий рядок";
+            Console.WriteLine("Рядок 2: " + text[1]);
+
+           
+            text.Clear();
+            Console.WriteLine("Кількість після очищення: " + text.LineCount);  
+        }
+    }
+    }
+
---
+++ DZ/DZ/TextContainer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ
+{
+    internal class TextContainer
+    {
+        private List<string> lines = new List<string>();
+
+       
+        public int LineCount
+        {
+            get { return lines.Count; }
+        }
+
+        
+        public string this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < lines.Count)
+                    return lines[index];
+                else
+                    throw new IndexOutOfRangeException("Недійсний індекс рядка.");
+            }
+            set
+            {
+                if (index >= 0 && index < lines.Count)
+                    lines[index] = value;
+                else
+                    throw new IndexOutOfRangeException("Недійсний індекс рядка.");
+            }
+        }
+
+        
+        public void AddLine(string line)
+        {
+            lines.Add(line);
+        }
+
+        
+        public void Clear()
+        {
+            lines.Clear();
+        }
+    }
+}
+
